625197a7b1ead1d68c42ffe1b29750eeae3525ce
Sigurd-Pettersen <73425444+Sigurd-Pettersen@users.noreply.github.com>
2022-09-07 22:49:07 +0200

    bug fixes, code optimizations and updates to UI

--- game.js
+++ game.js
@@ -5,7 +5,9 @@ const WIN = 0;
 const LOOSE = 1;
 const DRAW = 2;
 
-const status_msg = document.getElementById("status-msg");
+// const result_panel = document.getElementById("result-panel");
+const result_panel_msg = document.getElementById("result-panel-msg"); 
+const status_panel_msg = document.getElementById("status-panel-msg");
 const rock_button = document.getElementById("rock");
 const paper_button = document.getElementById("paper");
 const scissors_button = document.getElementById("scissors");
@@ -16,84 +18,82 @@ function getRandomNumber(range) {
 
 function choiceToString(choice) {
     switch (choice) {
-        case ROCK: return "rock";
-        case PAPER: return "paper";
-        case SCISSORS: return "scissors";
+        case ROCK: return "rock (👊)";
+        case PAPER: return "paper (📄)";
+        case SCISSORS: return "scissors (✂️)";
     }
 }
 
-function win(user_choice, bot_choice) {
-    let user_choice_str = choiceToString(user_choice);
-    let bot_choice_str = choiceToString(bot_choice);
-
-    status_msg.textContent  = `User won by using ${user_choice_str} against ${bot_choice_str}.`;
-    
-    let button_img = document.getElementById(user_choice_str).getElementsByTagName("img")[0];
-    button_img.style.borderColor = "#27eb79";
-    setTimeout(() => {button_img.style.borderColor = "#303030"}, 250)
+function choiceToEmoji(choice) {
+    switch (choice) {
+        case ROCK: return "👊";
+        case PAPER: return "📄";
+        case SCISSORS: return "✂️";
+    }
 }
 
-function loose(user_choice, bot_choice) {
+function showResult(user_choice, bot_choice, result) {
     let user_choice_str = choiceToString(user_choice);
     let bot_choice_str = choiceToString(bot_choice);
-    
-    status_msg.textContent = `User lost by using ${user_choice_str} against ${bot_choice_str}.`;
 
-    let button_img = document.getElementById(user_choice_str).getElementsByTagName("img")[0];
-    button_img.style.borderColor = "#c73939";
-    setTimeout(() => {button_img.style.borderColor = "#303030"}, 250)
+    console.log(`user_choice: ${user_choice_str}`);
+    console.log(`user_choice: ${bot_choice_str}`);
+    console.log(`result: ${result}`);
+    
+    let result_color = "";
+    let result_message = "";
+    let status_message = "";
+    
+    switch (result) {
+        case (WIN):
+            result_color = "#1ea155";
+            result_message = "Victory";
+            status_message = `User won over bot by using ${user_choice_str} against ${bot_choice_str}.`;
+            break;
+
+        case (LOOSE):
+            result_color = "#c73939";
+            result_message = "Defeat";
+            status_message = `User lost to bot by using ${user_choice_str} against ${bot_choice_str}.`;
+            break;
+            
+        case (DRAW):
+            result_color = "#303030";
+            result_message = "Draw";
+            status_message = `User drew with bot by using ${user_choice_str} against ${bot_choice_str}.`;
+            break;
+    }
+    
+    // result_panel.style.style.borderColor = result_color;
+    result_panel_msg.style.color = result_color;
+    result_panel_msg.textContent = result_message;
+    status_panel_msg.textContent = status_message;
 }
 
-function draw(user_choice, bot_choice) {
-    let user_choice_str = choiceToString(user_choice);
-    let bot_choice_str = choiceToString(bot_choice);
-    
-    status_msg.textContent = `User drew by using ${user_choice_str} against ${bot_choice_str}.`;
-    
-    let button_img = document.getElementById(user_choice_str).getElementsByTagName("img")[0];
-    button_img.style.borderColor = "#181818";
-    setTimeout(() => {button_img.style.borderColor = "#303030"}, 250)
+
+function getResult(user_choice, bot_choice) {
+    switch (user_choice) {
+        case (ROCK):
+            if (bot_choice === ROCK) {return DRAW;}
+            if (bot_choice === PAPER) {return LOOSE;}
+            if (bot_choice === SCISSORS) {return WIN;}
+        
+        case (PAPER):
+            if (bot_choice === ROCK) {return WIN;}
+            if (bot_choice === PAPER) {return DRAW;}
+            if (bot_choice === SCISSORS) {return LOOSE;}
+        
+        case (SCISSORS):
+            if (bot_choice === ROCK) {return LOOSE;}
+            if (bot_choice === PAPER) {return WIN;}
+            if (bot_choice === SCISSORS) {return DRAW;}
+    }
 }
 
 function game(user_choice) {
     let bot_choice = getRandomNumber(3);
-    
-    let result = 0;
-
-    switch (user_choice) {
-        case ROCK:
-            switch (bot_choice) {
-                case ROCK: result = DRAW; break;
-                case PAPER: result = LOOSE; break;
-                case SCISSORS: result = WIN; break;
-            }
-
-        case PAPER:
-            switch (bot_choice) {
-                case ROCK: result = WIN; break;
-                case PAPER: result = DRAW; break;
-                case SCISSORS: result = LOOSE; break;
-            }
-        
-        case SCISSORS:
-            switch (bot_choice) {
-                case ROCK: result = LOOSE; break;
-                case PAPER: result = WIN; break;
-                case SCISSORS: result = DRAW; break;
-            }
-    }
-
-    switch (result) {
-        case WIN:
-            win(user_choice, bot_choice);
-            break;
-        case LOOSE:
-            loose(user_choice, bot_choice);
-            break;
-        case DRAW:
-            draw(user_choice, bot_choice);
-            break;
-    }
+    let result = getResult(user_choice, bot_choice);
+    showResult(user_choice, bot_choice, result);
 }
 
 function main() {
